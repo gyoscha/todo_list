09c59b42e93f5d12a1aa0874077d3ede8089fcf7
gyoscha <sokolovgeorgy@gmail.com>
2022-05-28 17:34:29 +0300

    add views(not completed)

--- note_api/urls.py
+++ note_api/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('notes/', ...),
+    path('notes/', views.NoteListCreateAPIView.as_view()),
+    path('notes/<int:pk>', views.NoteDetailAPIView.as_view()),
 ]
--- note_api/views.py
+++ note_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
-# Create your views here.
+
+class NoteListCreateAPIView(ListCreateAPIView):
+    pass
+
+
+class NoteDetailAPIView(RetrieveUpdateDestroyAPIView):
+    pass
