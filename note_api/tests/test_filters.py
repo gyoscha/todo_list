714a7842e22e7afa9cb4075746f7cc4b3fa82a56
gyoscha <sokolovgeorgy@gmail.com>
2022-06-02 15:16:17 +0300

    template test filters

--- note_api/tests/test_filters.py
+++ note_api/tests/test_filters.py
@@ -15,13 +15,56 @@ class TestNoteFilter(TestCase):
     """
     @classmethod
     def setUpTestData(cls):
-        pass
+        User.objects.create_user(username='test_1', password='1234567')
+
+        Note.objects.create(
+            title='TEST_title_1',
+            note='TEST_msg_1',
+            author_id=1,
+            status='AC',
+            public=True,
+            important=True,
+        )
+        Note.objects.create(
+            title='TEST_title_2',
+            note='TEST_msg_2',
+            author_id=1,
+            status='PO',
+            public=False,
+            important=True,
+        )
+        Note.objects.create(
+            title='TEST_title_3',
+            note='TEST_msg_3',
+            author_id=1,
+            status='CO',
+            public=True,
+            important=False
+        )
+        Note.objects.create(
+            title='TEST_title_4',
+            note='TEST_msg_4',
+            author_id=1,
+            status='CO',
+            public=True,
+            important=True
+        )
+
+    def setUp(self) -> None:
+        """Перед каждым тестом логиниться"""
+        self.client.login(username='test_1', password='1234567')
 
     def test_filter_by_status(self):
-        pass
+        url = f'/api/v1/notes/?status=AC'
+
+    def test_filter_by_status_multi(self):
+        url = f'/api/v1/notes/?status=AC&status=CO'
 
     def test_filter_by_important(self):
-        pass
+        url = f'/api/v1/notes/?important=False'
 
     def test_filter_by_public(self):
-        pass
+        url = f'/api/v1/notes/?public=False'
+
+    def test_filter_all(self):
+        url = f'/api/v1/notes/?public=True&status=CO&important=True'
