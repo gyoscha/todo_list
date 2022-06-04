7bf74f748378851f690f5940f6fc76d22186755d
gyoscha <sokolovgeorgy@gmail.com>
2022-06-04 15:13:52 +0300

    add docs/

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('note_api.urls')),
-    path('', include('note.urls'))
+    path('', include('note.urls')),
+    path('docs/', schema_view.with_ui('swagger')),
 ]
